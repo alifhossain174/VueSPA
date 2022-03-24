cf23cb949abd930db61418819d3c098d8a10c0b6
Md. Fahim Hossain <alifhossain174@gmail.com>
2022-03-24 11:10:10 +0600

    first commit

---
+++ src/components/SignUp.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="logo_div">
+        <img alt="Laravel Logo" class="logo" src="../assets/laravel.png"> 
+        &nbsp;&nbsp;&nbsp;
+        <img alt="Vue logo" class="logo" src="../assets/logo.png" />
+    </div>
+    <h2>Sign Up Component</h2>
+</template>
+
+<script>
+export default {
+    name: 'SignUp'
+}
+</script>
+
+<style>
+    .logo_div{
+        width: 100%;
+        text-align: center;
+    }
+    .logo{
+        width: 100px;
+    }
+</style>
